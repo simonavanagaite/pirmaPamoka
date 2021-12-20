d24784ded4ad015e49e5adbc90fd993b4bae1225
unknown <simona.vanagaite@gmail.com>
2021-12-20 21:18:15 +0200

    Created a new application

---
+++ PirmaPamoka/PirmaPamoka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PirmaPamoka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
